9b725f5ce20c90ef298947cac63948cc8c19cb41
Joe Collins <sportyguy_007@hotmail.com>
2012-02-11 21:40:26 -0500

    Initial Commit

---
+++ ContainerViewController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ContainerViewController
+//
+//  Created by Joseph Collins on 2/11/12.
+//  Copyright (c) 2012 Joseph Collins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ContainerViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContainerViewController
+//
+//  Created by Joseph Collins on 2/11/12.
+//  Copyright (c) 2012 Joseph Collins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
